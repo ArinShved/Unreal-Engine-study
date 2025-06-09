f095d5173249cd509a4c07466588ac1915a90676
Arina Shvedova <arina4shvedova@gmail.com>
2025-06-09 23:04:24 +0300

    Add SinMovement

---
+++ Cpp_base_1/Source/Cpp_base_1/Private/CPPHubActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPHubActor.h"
+
+// Sets default values
+ACPPHubActor::ACPPHubActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPPHubActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPPHubActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
